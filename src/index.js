5c6185077136d2bd6b48edbdcad9324fc575be3e
aleks1012 <aleksei.zharskii@gmail.com>
2021-03-06 16:03:11 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+const a = -n;
+if (n > 0) {
+  return n.toString().split('').reverse().join('');  
+					} else {
+  							return a.toString().split('').reverse().join('');
+							}
 }
